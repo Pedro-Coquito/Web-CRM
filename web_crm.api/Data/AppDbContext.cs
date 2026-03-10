c1bf1fdc27719caf993e5ac64a11c94f981e608b
Pedro-Coquito <sixsecond3@gmail.com>
2026-03-10 15:45:46 -0300

    Adiciona entidades Fornecedor e Documento ao AppDbContext
    Inclui DbSets para FornecedorModel e FornecedorDocModels, converte enum TipoDepessoa para string e configura relação 1:1 entre fornecedor e documento no método OnModelCreating.

--- web_crm.api/Data/AppDbContext.cs
+++ web_crm.api/Data/AppDbContext.cs
@@ -12,6 +12,19 @@ namespace web_crm.api.Data {
 
         public DbSet<ProdutosModel> produtos { get; set; }
         public DbSet<UsuarioModel> usuarios { get; set; }
+
+        public DbSet<FornecedorModel> fornecedor { get; set; }
+        public DbSet<FornecedorDocModels> fornecedor_documentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            //converte enum para string
+            modelBuilder.Entity<FornecedorModel>().Property(e => e.TipoDepessoa).HasConversion<string>();
+
+            //configura relação 1:1 
+            modelBuilder.Entity<FornecedorModel>().HasOne(e => e.Documento).WithOne(d => d.Fornecedor).HasForeignKey<FornecedorDocModels>(d => d.EntidadeId);
+        }
     }
 }
 
