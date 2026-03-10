94f286b8d1e745f55d9d1a204d7fb43102d78137
Pedro-Coquito <sixsecond3@gmail.com>
2026-03-10 15:47:02 -0300

    Configuração do Model Fornecedor

---
+++ web_crm.api/Models/FornecedorModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace web_crm.api.Models {
+
+    public enum Tipo_pessoa {
+        Fisca,
+        Juridica
+    }
+
+    [Table("entidades")]
+    public class FornecedorModel {
+        [Key]
+        [Column("id")]
+        public int id { get; set; }
+
+        [Required]
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Column("contribuinte_id")]
+        public int ContribuinteId { get; set; }
+
+        [Column("Codigo")]
+        public string Codigo { get; set; }
+
+        [Column("tipo_pessoa")]
+        public Tipo_pessoa TipoDepessoa{ get; set; }
+
+        [Column("cliente_desde")]
+        public DateTime ClienteDesde { get; set; }
+
+        [Column("criado_em")]
+        public DateTime CriadoEm {  get; set; }
+
+
+        public FornecedorDocModels Documento{ get; set; }
+
+
+    }
+}
