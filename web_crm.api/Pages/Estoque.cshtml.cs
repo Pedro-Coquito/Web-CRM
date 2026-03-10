aae75da68f23f2a8d35989989f3d354e4a01f189
Pedro-Coquito <sixsecond3@gmail.com>
2026-03-10 14:26:53 -0300

    Implementa página de gerenciamento de estoque de produtos
    Adiciona Estoque.cshtml e Estoque.cshtml.cs para exibição e cadastro de produtos via modal. Atualiza _Layout.cshtml com menu de navegação para estoque e placeholders para cadastro e relatório de produtos. Cria arquivos básicos para a página inicial. Pequeno erro de sintaxe identificado no divider do menu.

---
+++ web_crm.api/Pages/Estoque.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web_crm.api.Pages {
+    // O nome da classe DEVE ser EstoqueModel
+    public class EstoqueModel : PageModel {
+        public void OnGet() {
+            // Este método roda automaticamente quando você entra na página de estoque
+        }
+    }
+}
---
+++ web_crm.api/Pages/Shared/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web_crm.api.Pages.Shared
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
