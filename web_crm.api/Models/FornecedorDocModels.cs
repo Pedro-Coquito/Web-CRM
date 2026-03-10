62ff583932bfe474dc6589045c6099bc70bf2e57
Pedro-Coquito <sixsecond3@gmail.com>
2026-03-10 15:46:41 -0300

    Criação do Model de cadastro de documentos do fornecedor

---
+++ web_crm.api/Models/FornecedorDocModels.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace web_crm.api.Models {
+
+    [Table("entidades_documentos")]
+    public class FornecedorDocModels {
+
+        [Key]
+        [Column("entidade_id")]
+        public int EntidadeId { get; set; }
+
+        [Column("nome_fantasia")]
+        public string NomeFantasia { get; set; }
+
+        [Column("cpf_cnpj")]
+        public string Cpf_Cnpj {  get; set; }
+
+        [Column("rg")]
+        public string Rg {  get; set; }
+
+        [Column("orgao_emissor")]
+        public string OrgaoEmissor {  get; set; }
+
+        [Column("inscricao_estadual")]
+        public string InscricaoEstadual {  get; set; }
+
+
+
+        [ForeignKey("entidade_id")]
+        public FornecedorModel Fornecedor { get; set; }
+    }
+}
